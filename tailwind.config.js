abdda48457c2842769e2574c2bc48549adcc02bc
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-11 16:22:37 -0300

    tailwind theme modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Ubuntu', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'marine-blue': 'hsl(213, 96%, 18%)',
+        'purplish-blue': 'hsl(243, 100%, 62%)',
+        'pastel-blue': 'hsl(228, 100%, 84%)',
+        'light-blue': 'hsl(206, 94%, 87%)',
+        'strawberry-red': 'hsl(0, 100%, 63%)',
+        'cool-gray': 'hsl(231, 11%, 63%)',
+        'light-gray': 'hsl(229, 24%, 87%)',
+        'magnolia': 'hsl(217, 100%, 97%)',
+        'alabaster': 'hsl(231, 100%, 99%)',
+      },
+      backgroundImage: {
+        'sidebar-mobile': "url('/bg-sidebar-mobile.svg')",
+        'sidebar-desktop': "url('/bg-sidebar-desktop.svg')",
+    },
+    },
   },
   plugins: [],
 }
