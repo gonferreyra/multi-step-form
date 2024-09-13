6a4171af2ec7f703aede25d797c5ee7704d3c324
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-13 17:29:07 -0300

    constants.ts created

---
+++ src/lib/constants.ts
@@ -0,0 +1,26 @@
+export const plans = [
+  {
+    icon: '/icon-arcade.svg',
+    name: 'Arcade',
+    priceMonth: 9,
+    priceYear: 90,
+    // interval: 'month',
+    // selected: true,
+  },
+  {
+    icon: '/icon-advanced.svg',
+    name: 'Advanced',
+    priceMonth: 12,
+    priceYear: 120,
+    // interval: 'month',
+    // selected: false,
+  },
+  {
+    icon: '/icon-pro.svg',
+    name: 'Pro',
+    priceMonth: 15,
+    priceYear: 150,
+    // interval: 'month',
+    // selected: false,
+  },
+];
