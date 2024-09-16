3c897f5294c1b9d7c8f35c12453b9cd8825f10da
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-16 16:43:28 -0300

    4th step and functions to calculate total amount working on mobile devices

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,31 +6,36 @@ import { addOns, plans } from '../lib/constants';
 import PlanCard from '../ui/PlanCard';
 import PlanSwitch from '../ui/PlanSwitch';
 import AddOnCard from '../ui/AddOnCard';
+import clsx from 'clsx';
 
 export default function Form() {
-  const activeStep = useActiveId();
-  const [planTime, setPlanTime] = useState('month');
+  const { activeStep, handleActiveStep } = useActiveId();
+  const [planTime, setPlanTime] = useState('monthly');
   const [isChecked, setIsChecked] = useState(false);
 
   //  second step
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
 
   // third step
   const [selectedAddOns, setSelectedAddOns] = useState<string[]>([]);
+  // console.log(selectedAddOns);
 
+  // month or year toggle
   const handleToggle = () => {
     setIsChecked(!isChecked);
-    setPlanTime(isChecked ? 'month' : 'year');
+    setPlanTime(isChecked ? 'monthly' : 'yearly');
   };
 
-  const handleTogglePlan = (option: string) => {
-    if (option !== selectedOption) {
-      setSelectedOption(option);
+  // second step
+  const handleSelectPlan = (option: string) => {
+    if (option !== selectedPlan) {
+      setSelectedPlan(option);
     } else {
-      setSelectedOption(null);
+      setSelectedPlan(null);
     }
   };
 
+  // third step
   const handleSelectAddOn = (name: string, selected: boolean) => {
     if (selected) {
       setSelectedAddOns([...selectedAddOns, name]);
@@ -38,6 +43,36 @@ export default function Form() {
       setSelectedAddOns(selectedAddOns.filter((addOn) => addOn !== name));
     }
   };
+
+  // const calculateAddOnsPrice = () => {
+  //   return selectedAddOns.reduce((total, addOn) => {
+  //     const selectedAddOn = addOns.find(a => a.name === addOn);
+  //     if (selectedAddOn) {
+  //       return total + (planTime === 'monthly' ? selectedAddOn.priceMonth : selectedAddOn.priceYear);
+  //     }
+  //     return total;
+  //   }, 0);
+  // };
+
+  const getAddOnPrice = (addOnName: string) => {
+    const addOn = addOns.find((a) => a.name === addOnName);
+    if (addOn) {
+      return planTime === 'monthly' ? addOn.priceMonth : addOn.priceYear;
+    }
+    return 0;
+  };
+
+  const getPlanPrice = () => {
+    const plan = plans.find((a) => a.name === selectedPlan);
+    if (plan) {
+      return planTime === 'monthly' ? plan.priceMonth : plan.priceYear;
+    }
+    return 0;
+  };
+
+  const total =
+    getPlanPrice() +
+    selectedAddOns.reduce((total, addOn) => total + getAddOnPrice(addOn), 0);
 
   return (
     <form className="absolute -top-20 left-0 right-0 flex min-h-[calc(100vh-120px)] flex-col gap-6">
@@ -111,8 +146,8 @@ export default function Form() {
               key={plan.name}
               {...plan}
               planTime={planTime}
-              isSelected={selectedOption === plan.name}
-              onToggle={() => handleTogglePlan(plan.name)}
+              isSelected={selectedPlan === plan.name}
+              onToggle={() => handleSelectPlan(plan.name)}
             />
           ))}
 
@@ -134,12 +169,75 @@ export default function Form() {
               key={addon.name}
               {...addon}
               planTime={planTime}
+              selected={selectedAddOns.includes(addon.name)}
               onSelect={handleSelectAddOn}
             />
           ))}
         </div>
       )}
 
+      {/* Step 4 */}
+      {activeStep === 4 && (
+        <div className="mx-auto flex w-[90%] max-w-[500px] flex-col gap-4 rounded-xl bg-alabaster px-4 py-6">
+          <FormTitle>Finishing up</FormTitle>
+          <FormSubtitle>
+            Double-check everything looks <span className="uppercase">ok</span>{' '}
+            before confirming.
+          </FormSubtitle>
+
+          <div className="rounded-lg bg-magnolia p-4">
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="font-semibold text-marine-blue">
+                  {selectedPlan}{' '}
+                  <span className="capitalize">({planTime})</span>
+                </p>
+                <button
+                  type="button"
+                  className="text-sm font-semibold text-cool-gray underline"
+                  onClick={() => handleActiveStep(2)}
+                >
+                  Change
+                </button>
+              </div>
+              <div>
+                <p className="text-sm font-semibold text-marine-blue">
+                  ${getPlanPrice()}/{planTime === 'yearly' ? 'yr' : 'mo'}
+                </p>
+              </div>
+            </div>
+            <div className="mb-4 mt-4 border-t" />
+            <div className="">
+              {selectedAddOns.map((addOn) => (
+                <div
+                  key={addOn}
+                  className="mt-4 flex items-center justify-between"
+                >
+                  <p className="text-sm font-semibold text-cool-gray">
+                    {addOn}
+                  </p>
+                  <p className="text-sm text-marine-blue">
+                    +${getAddOnPrice(addOn)}/
+                    {planTime === 'yearly' ? 'yr' : 'mo'}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-3 flex items-center justify-between px-4">
+            <p className="text-sm font-semibold text-cool-gray">
+              Total (per month)
+            </p>
+            <p className="font-semibold text-purplish-blue">
+              +${total}/{planTime === 'yearly' ? 'yr' : 'mo'}
+            </p>
+          </div>
+        </div>
+      )}
+
+      {/* Step 5 */}
+
+      {/* Buttons */}
       <div className="mt-auto flex w-full items-center justify-between bg-alabaster px-2">
         {activeStep > 1 && (
           <a
@@ -151,10 +249,15 @@ export default function Form() {
         )}
 
         <a
-          href={`#${activeStep + 1}`}
-          className="my-4 ml-auto rounded-md bg-marine-blue p-3 font-bold text-alabaster"
+          href={`#${activeStep < 5 ? activeStep + 1 : 4}`}
+          className={clsx(
+            'my-4 ml-auto rounded-md bg-marine-blue p-3 font-bold text-alabaster',
+            {
+              'bg-purplish-blue': activeStep === 4,
+            },
+          )}
         >
-          Next Step
+          {activeStep === 4 ? 'Confirm' : 'Next Step'}
         </a>
       </div>
     </form>
