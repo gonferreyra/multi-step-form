043e8339d6adb78efed2bdf98fefcfd5c30ba7f2
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-17 11:50:49 -0300

    barrel file created

---
+++ src/components/FormSteps/index.ts
@@ -0,0 +1,6 @@
+// export all FormSteps components
+export { default as FormStep1 } from './FormStep1';
+export { default as FormStep2 } from './FormStep2';
+export { default as FormStep3 } from './FormStep3';
+export { default as FormStep4 } from './FormStep4';
+export { default as FormStep5 } from './FormStep5';
