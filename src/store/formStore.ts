174220bbdf4e3f531c359eff6b1eccc8b0cd1625
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-23 10:23:48 -0300

    added new states to global store

--- src/store/formStore.ts
+++ src/store/formStore.ts
@@ -4,6 +4,10 @@ import createSelectors from './selectors';
 type FormStore = {
   activeStep: number;
   setActiveStep: (step: number) => void;
+  planTime: 'monthly' | 'yearly';
+  setPlanTime: (string: 'monthly' | 'yearly') => void;
+  isChecked: boolean;
+  setIsChecked: () => void;
 };
 
 const useFormStore = create<FormStore>()((set) => ({
@@ -12,6 +16,20 @@ const useFormStore = create<FormStore>()((set) => ({
     set((state) => ({
       ...state,
       activeStep: step,
+    }));
+  },
+  planTime: 'monthly',
+  setPlanTime: (string) => {
+    set((state) => ({
+      ...state,
+      planTime: string,
+    }));
+  },
+  isChecked: false,
+  setIsChecked: () => {
+    set((state) => ({
+      ...state,
+      isChecked: !state.isChecked,
     }));
   },
 }));
